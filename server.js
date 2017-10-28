fe4d9b59534f2b563a741bf70e4954e47c52a34f
Ankit Arora <ankit992@live.in>
2017-10-29 01:25:41 +0530

    learning about partials and helper methods

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ hbs.registerPartials(__dirname + '/views/partials' );
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/',(req, res) => {
     // res.send('<h1>Hello Express!</h1>');
     // res.send({
@@ -16,7 +24,6 @@ app.get('/',(req, res) => {
     res.render('home',{
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to my website',        
-        currentYear: new Date().getFullYear()
     });
 });
 
