0d56e5d5fec4505c96802391f206dde1de42ebb3
Ankit Arora <ankit992@live.in>
2017-10-29 00:50:32 +0530

    Adding first html page in express and adding default express middleware

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req, res) => {
     // res.send('<h1>Hello Express!</h1>');
     res.send({
@@ -14,4 +16,9 @@ app.get('/about',(req, res) => {
     res.send('About page');
 });
 
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'This is an error message'
+    });
+});
 app.listen(3000);
